22f40ad8a4307f78dca50d8a178217e4b70b32bf
poltokogh <147163884+poltokogh@users.noreply.github.com>
2023-10-28 22:40:25 +0700

    Add files via upload

---
+++ python/python-arithmatic-operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    tambah = a + b
+    kurang = a - b
+    kali = a * b
+
+    print(tambah)
+    print(kurang)
+    print(kali)
---
+++ python/python-division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    bagi1 = a//b
+    bagi2 = a / b
+    print(bagi1)
+    print(bagi2)
